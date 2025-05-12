cd5cdec311f047291c84f7eac6c6f83ce35df643
tonguyenducmanh <ducmanh14032000@gmail.com>
2025-05-12 21:34:58 +0700

    bổ sung lưu expiretime với cache mã hóa

--- src/common/TDCache.js
+++ src/common/TDCache.js
@@ -79,26 +79,43 @@ class TDCache {
     const storage = this.getStorage(config.CacheLevel);
     const valueStr = typeof value === "string" ? value : JSON.stringify(value);
     const encrypted = CryptoJS.AES.encrypt(valueStr, password).toString();
-    storage.setItem(key, encrypted);
+    const payload = {
+      value: encrypted,
+      expiredAt:
+        config.ExpireTime > 0 ? Date.now() + config.ExpireTime * 1000 : null,
+    };
+    storage.setItem(key, JSON.stringify(payload));
   }
 
   getEncrypted(configKey, password, params = {}) {
     const config = this.getCacheConfigByKey(configKey);
+    let result = null;
     if (!config) throw new Error(`Không tìm thấy cấu hình cache: ${configKey}`);
 
     const key = this.formatKey(config.KeyFormat, params);
     const storage = this.getStorage(config.CacheLevel);
     const encrypted = storage.getItem(key);
-    if (!encrypted) return null;
+    if (!encrypted) return result;
 
     try {
-      const bytes = CryptoJS.AES.decrypt(encrypted, password);
-      const decrypted = bytes.toString(CryptoJS.enc.Utf8);
-      return JSON.parse(decrypted);
+      const { value, expiredAt } = JSON.parse(encrypted);
+      if (expiredAt && Date.now() > expiredAt) {
+        storage.removeItem(key);
+        return result;
+      }
+      if (value) {
+        const bytes = CryptoJS.AES.decrypt(value, password);
+        const decrypted = bytes.toString(CryptoJS.enc.Utf8);
+        result = JSON.parse(decrypted);
+        if (result == "[]") {
+          result = [];
+        }
+      }
     } catch (e) {
       console.error("Lỗi giải mã: Sai mật khẩu hoặc dữ liệu lỗi", e);
-      return null;
+      return result;
     }
+    return result;
   }
 }
 
