b3d3cb98781ff1ac9174a19dd9e1ddc0c802f554
tonguyenducmanh <ducmanh14032000@gmail.com>
2025-06-04 20:36:33 +0700

    bố sung validate formatted code

--- src/common/formatter/postgresql/postgreSQLFormatter.js
+++ src/common/formatter/postgresql/postgreSQLFormatter.js
@@ -10,5 +10,23 @@ export function formatCode(sourceCode) {
   let tokens = generateTokenizer(sourceCode);
   let ast = generateParser(tokens);
   let formattedCode = codeGenerator(ast);
-  return formattedCode;
+  if (validateCode(sourceCode, formattedCode)) {
+    return formattedCode;
+  } else {
+    console.error("Formatted code does not match the original source code.");
+    return null;
+  }
+}
+
+/**
+ * kiểm tra xem code đã format có đúng với code ban đầu không
+ * @param {String} sourceCode sql ban đầu
+ * @param {String} formattedCode sql sau khi format
+ * @returns
+ */
+function validateCode(sourceCode, formattedCode) {
+  // lọc hết khoảng cách giữa các chữ và xuống dòng đi
+  let sourceCodeTrim = sourceCode.replace(/\s+/g, "");
+  let formatCodeTrim = formattedCode.replace(/\s+/g, "");
+  return sourceCodeTrim.compareText(formatCodeTrim);
 }
