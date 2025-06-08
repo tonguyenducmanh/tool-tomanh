7faf68554a01386d3d37bdb7e5bbeb90943522c9
tonguyenducmanh <ducmanh14032000@gmail.com>
2025-06-08 07:55:26 +0700

    update config

--- public/cfg/config.js
+++ public/cfg/config.js
@@ -15,37 +15,11 @@
   window.__env.githubSource = {
     url: "https://github.com/tonguyenducmanh/utility-for-dev",
   };
-
   window.__env.jsonParser = {
     showConfigLib: false,
   };
-
   window.__env.oneTimePasswordAuthen = {
     showDecodedInfo: false,
     autoSave: true,
-  };
-  window.__env.postgreSQLFormatter = {
-    // có tự động viết hoa các từ khóa trong postgreSQL không
-    usingUpperCaseKeyWord: true,
-    // độ dài của 1 tab
-    tabSpace: "    ",
-    // danh sách các từ khóa sẽ xuống dòng và tab vào
-    listKeyWordBreakLine: [
-      "select",
-      "where",
-      "having",
-      "and",
-      "or",
-      "from",
-      "drop",
-      "delete",
-      "join",
-      "update",
-      "truncate",
-      "begin",
-      "end",
-    ],
-    // danh sách các cụm từ khóa sẽ xuống dòng và tab vào
-    listMutipleKeyWordBreakLine: ["group by", "order by", "left join"],
   };
 })(this);
