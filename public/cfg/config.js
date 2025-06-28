3f72c0fe8bfb2e367b9d66fcd381af0d20934c21
tonguyenducmanh <ducmanh14032000@gmail.com>
2025-06-28 15:00:11 +0700

    nâng version

--- public/cfg/config.js
+++ public/cfg/config.js
@@ -3,7 +3,7 @@
  */
 (function (window) {
   window.__env = window.__env || {};
-  window.__env.version = "v1.1.6";
+  window.__env.version = "v1.1.7";
   window.__env.defaultValue = {
     theme: "light",
   };
