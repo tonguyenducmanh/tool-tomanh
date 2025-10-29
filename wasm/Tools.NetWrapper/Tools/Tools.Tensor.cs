d8f0929e7f1782dba31c25be6fae25089c8ac76c
tonguyenducmanh <ducmanh14032000@gmail.com>
2025-10-29 23:24:36 +0700

    hoàn thiện tool tensor

--- wasm/Tools.NetWrapper/Tools/Tools.Tensor.cs
+++ wasm/Tools.NetWrapper/Tools/Tools.Tensor.cs
@@ -1,10 +1,18 @@
 using System.Numerics.Tensors;
 using System.Runtime.InteropServices.JavaScript;
-using Newtonsoft.Json;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 namespace Tools.NetWrapper
 {
+
+    /// <summary>
+    /// phải có class này để hỗ trợ System.Text.Json source generator
+    /// </summary>
+    [JsonSerializable(typeof(CosinSimilarityInput))]
+    public partial class AppJsonContext : JsonSerializerContext
+    {
+    }
+
     /// <summary>
     /// Class chứa danh sách các hàm tiện ích liên quan đến Tensor
     /// </summary>
@@ -13,25 +21,38 @@ namespace Tools.NetWrapper
         [JSExport]
         public static string CosinSimilarity(string source)
         {
+            //return source + " - from ToolsTensor";
             if (string.IsNullOrEmpty(source))
             {
                 throw new ArgumentNullException(nameof(source));
             }
-            var input = JsonConvert.DeserializeObject<CosinSimilarityInput>(source);
+            // không deserialize trực tiếp được vì thiếu tham chiếu System.Text.Json source generator
+            var input = (CosinSimilarityInput)JsonSerializer.Deserialize(source, typeof(CosinSimilarityInput), AppJsonContext.Default);
 
-            if (input == null || input.DataA == null || input.DataB == null)
+            if (input == null || input.FirstVector == null || input.SecondVector == null)
             {
                 throw new ArgumentException("Invalid input data");
             }
             // Tạo hai tensor mẫu
-            var result = TensorPrimitives.CosineSimilarity(input.DataA, input.DataB);
+            var result = TensorPrimitives.CosineSimilarity(input.FirstVector, input.SecondVector);
             return result.ToString();
         }
 
-        private class CosinSimilarityInput
-        {
-            public float[] DataA { get; set; }
-            public float[] DataB { get; set; }
-        }
+    }
+
+    /// <summary>
+    /// Class biểu diễn dữ liệu đầu vào cho hàm CosinSimilarity
+    /// </summary>
+    public class CosinSimilarityInput
+    {
+        /// <summary>
+        /// Mảng dữ liệu thứ nhất
+        /// </summary>
+        public float[]? FirstVector { get; set; }
+
+        /// <summary>
+        /// Mảng dữ liệu thứ hai
+        /// </summary>
+        public float[]? SecondVector { get; set; }
     }
 }
