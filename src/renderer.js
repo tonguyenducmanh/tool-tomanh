e3584385053c815349eadee1338a7442e27df58d
tonguyenducmanh <ducmanh14032000@gmail.com>
2025-10-29 23:29:39 +0700

    add prebuild dotnet file

--- src/renderer.js
+++ src/renderer.js
@@ -15,6 +15,8 @@ import TDHistory from "@/components/TDHistory.vue";
 import i18nData, { loadLocale } from "@/i18n/i18nData.js";
 import eventBus from "@/common/event/TDEventBus.js";
 import ToastPlugin from "@/common/TDToastUtil.js";
+// import blazor webassembly
+import { dotnet } from "/public/_framework/dotnet.js";
 
 // Async IIFE
 (async () => {
@@ -51,4 +53,39 @@ import ToastPlugin from "@/common/TDToastUtil.js";
   await loadLocale(locale);
 
   currentApp.mount("#app");
+  const runtime = await dotnet.create();
+  runtime.setModuleImports("globalThis", globalThis);
+
+  // demo gọi hàm từ blazor wasm
+  // const { getAssemblyExports } = runtime;
+  // const exports = await getAssemblyExports("Tools.NetWrapper.dll");
+  // const { ToolsTensor } = exports.Tools.NetWrapper;
+  // // 1. Chuẩn bị đối tượng JavaScript
+  // const inputData = {
+  //   // Mảng float (Number trong JS)
+  //   FirstVector: [1.0, 2.0, 3.0, 4.0],
+  //   SecondVector: [5.0, 6.0, 7.0, 8.0],
+  // };
+
+  // // 2. Marshalling: Chuyển đối tượng JS thành chuỗi JSON
+  // const jsonString = JSON.stringify(inputData);
+
+  // try {
+  //   // 3. Gọi hàm C# đã export
+  //   const resultString = ToolsTensor.CosinSimilarity(jsonString);
+
+  //   // 4. Xử lý kết quả (Vì C# trả về float.ToString() là một chuỗi)
+  //   const similarity = parseFloat(resultString);
+
+  //   console.log("Input Data:", inputData);
+  //   console.log("JSON Sent to C#:", jsonString);
+  //   console.log("Result (string from C#):", resultString);
+  //   console.log("Cosin Similarity:", similarity); // Kết quả là một số (float)
+  // } catch (error) {
+  //   console.error("Lỗi khi gọi CosinSimilarity từ C#:", error);
+  // }
+  // debugger;
+
+  // gán runtime vào window để các module khác có thể dùng
+  window.__dotnet_runtime = runtime;
 })();
