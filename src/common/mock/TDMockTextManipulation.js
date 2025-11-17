d6142f5a9daa8dea47e7b9e5ef79ce6bd8f245e9
tonguyenducmanh <ducmanh14032000@gmail.com>
2025-11-17 19:43:26 +0700

    .

--- src/common/mock/TDMockTextManipulation.js
+++ src/common/mock/TDMockTextManipulation.js
@@ -1,4 +1,10 @@
 export default {
-  inputSource: "",
-  expressionSource: "",
+  inputSource: `Lumbergh, Bill, Initech
+Waddams, Milton, Initech
+Gibbons, Peter, Initech
+Bolton, Michael, Initech`,
+  expressionSource: `To: $1.$0@$2.com
+Hello $1 $0,
+I'm sorry to inform you of a terrible accident at $2.
+---`,
 };
