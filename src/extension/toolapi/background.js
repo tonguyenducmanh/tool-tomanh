5456f8b99e65c22458ab643294f4c8c8ee294a3f
tonguyenducmanh <ducmanh14032000@gmail.com>
2025-12-13 21:44:30 +0700

    .

--- src/extension/toolapi/background.js
+++ src/extension/toolapi/background.js
@@ -1,14 +1,16 @@
 // Background Service Worker - Handles API requests and CORS bypass
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "callAPI") {
-    handleAPICall(request.data).then(sendResponse).catch((error) => {
-      sendResponse({
-        success: false,
-        error: error.message,
-        status: null,
-        body: null,
+    handleAPICall(request.data)
+      .then(sendResponse)
+      .catch((error) => {
+        sendResponse({
+          success: false,
+          error: error.message,
+          status: null,
+          body: null,
+        });
       });
-    });
     // Return true to indicate we will send a response asynchronously
     return true;
   }
@@ -20,7 +22,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
  * @param {string} data.url - API endpoint URL
  * @param {string} data.method - HTTP method (GET, POST, PUT, DELETE, etc.)
  * @param {Object} data.headers - Request headers
- * @param {string} data.body - Request body (for POST, PUT, PATCH)
+ * @param {string} data.body - Request body
  * @returns {Promise<Object>} Response object
  */
 async function handleAPICall(data) {
@@ -36,7 +38,7 @@ async function handleAPICall(data) {
   };
 
   // Add body for applicable methods
-  if (body && ["POST", "PUT", "PATCH"].includes(options.method)) {
+  if (body) {
     options.body = body;
   }
 
