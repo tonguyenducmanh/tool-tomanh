faa5915630096e1b68cbb40430077e494bab1749
tonguyenducmanh <ducmanh14032000@gmail.com>
2026-02-09 19:52:53 +0700

    chuyển qua thuần lcoal host

--- src_backend/td_core_service/internal/core_service/bl_core_service.go
+++ src_backend/td_core_service/internal/core_service/bl_core_service.go
@@ -3,33 +3,14 @@ package core_service
 import (
 	"crypto/rand"
 	"fmt"
-	"net"
 )
-
-// GetLocalIP trả về địa chỉ IP nội bộ đầu tiên mà nó tìm thấy
-func GetLocalIP() string {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		return "localhost"
-	}
-	for _, address := range addrs {
-		// Kiểm tra xem nó có phải là một IP network không
-		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			// Kiểm tra nếu là IPv4
-			if ipnet.IP.To4() != nil {
-				return ipnet.IP.String()
-			}
-		}
-	}
-	return "localhost"
-}
 
 /**
  * log ra server đang chạy
  */
 func BuildRunningAddressServer(server_name string, port *int) string {
 	addr := fmt.Sprintf("%d", *port)
-	server := GetLocalIP()
+	server := "localhost"
 	fmt.Printf("%s đang chạy tại http://%s:%s\n", server_name, server, addr)
 	addBuildMuxServer := fmt.Sprintf(":%d", *port)
 	return addBuildMuxServer
