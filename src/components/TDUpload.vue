bb8e3fb4330a1f1fad2dd8d7596c2d178b2efb0b
tonguyenducmanh <ducmanh14032000@gmail.com>
2026-03-15 16:35:49 +0700

    .

--- src/components/TDUpload.vue
+++ src/components/TDUpload.vue
@@ -77,15 +77,9 @@ export default {
       let me = this;
       let result = "";
       if (me.selectedFiles && me.selectedFiles.length > 0) {
-        if (me.selectedFiles.length == 1) {
-          result = `${me.selectedFiles[0].name} (${me.formatFileSize(
-            me.selectedFiles[0].size,
-          )})`;
-        } else {
-          result = `${me.$t("i18nCommon.fileSelected")}: ${
-            me.selectedFiles.length
-          }`;
-        }
+        result = `${me.$t("i18nCommon.fileSelected")}: ${
+          me.selectedFiles.length
+        }`;
       }
       return result;
     },
