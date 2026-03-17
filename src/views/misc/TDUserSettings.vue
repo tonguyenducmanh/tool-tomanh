75daa28b0ceb6dadbe025a3486bf1c94ce7a5fcf
tonguyenducmanh <ducmanh14032000@gmail.com>
2026-03-17 21:12:51 +0700

    rollback vì lỡ xóa đi vài cái quan trọng

--- src/views/misc/TDUserSettings.vue
+++ src/views/misc/TDUserSettings.vue
@@ -2,6 +2,17 @@
   <div class="flex td-user-setting">
     <div class="flex flex-col setting-group">
       <div class="flex user-setting-item">
+        <div>{{ $t("i18nCommon.apiTesting.tooltipUrlAgent") }}</div>
+        <div class="input-setting">
+          <TDInput
+            v-model="currentUserSetting.agentURL"
+            v-tooltip="$t('i18nCommon.apiTesting.tooltipUrlAgent')"
+            :noMargin="true"
+            :placeHolder="$t('i18nCommon.apiTesting.agentUrl')"
+          />
+        </div>
+      </div>
+      <div class="flex user-setting-item">
         <div>{{ $t("i18nUserSettings.settings.theme") }}</div>
         <TDComboBox
           :width="200"
@@ -33,9 +44,34 @@
         :label="$t('i18nUserSettings.saveSetting')"
       />
     </div>
+    <div class="divide"></div>
+
+    <div class="flex flex-col setting-group">
+      <div class="flex user-setting-item">
+        <div>{{ $t("i18nCommon.apiTesting.pingAgent") }}</div>
+        <TDButton
+          :noMargin="true"
+          @click="heathCheck"
+          :type="$tdEnum.buttonType.secondary"
+          :label="$t('i18nCommon.ping')"
+        ></TDButton>
+      </div>
+      <div class="flex user-setting-item">
+        <div>{{ $t("i18nCommon.apiTesting.downloadAgent") }}</div>
+        <TDButton
+          :noMargin="true"
+          :type="$tdEnum.buttonType.secondary"
+          @click="goToSource"
+          :label="$t('i18nCommon.tdheader.goToSource')"
+          v-tooltip="$t('i18nCommon.apiTesting.toolTipDownloadAgent')"
+        />
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import TDCURLUtil from "@/common/api/CURLHandle/TDCURLUtil.js";
+import TDAgentAPI from "@/common/api/request/AgentAPI/TDAgentAPI.js";
 import { getUserSettingDefault } from "@/common/TDUserSettingDefault.js";
 
 export default {
@@ -60,6 +96,19 @@ export default {
     me.processWhenCreated();
   },
   methods: {
+    async heathCheck() {
+      let me = this;
+      try {
+        let res = await new TDAgentAPI().heathCheck();
+        if (res && res.success && res.data) {
+          me.$tdToast.success(res.data);
+        } else {
+          me.$tdToast.success(res);
+        }
+      } catch (ex) {
+        me.$tdUtility.showErrorNotFoundAgentServer();
+      }
+    },
     async processWhenCreated() {
       let me = this;
       let cacheData = await me.$tdUtility.getUserSettings();
@@ -67,14 +116,25 @@ export default {
         me.currentUserSetting = Object.assign(me.currentUserSetting, cacheData);
       }
     },
+    goToSource() {
+      let me = this;
+      me.$tdUtility.goToSource();
+    },
     async saveSetting() {
       let me = this;
       me.saveTheme();
+      me.handleChangeAgentURL();
       await me.$tdCache.set(
         me.$tdEnum.cacheConfig.UserSettings,
         me.currentUserSetting,
       );
       me.$tdUtility.reloadApp();
+    },
+    handleChangeAgentURL() {
+      let me = this;
+      TDCURLUtil.setGlobalInfoBeforeRequest({
+        agentURL: me.currentUserSetting.agentURL,
+      });
     },
     saveTheme() {
       let me = this;
@@ -89,15 +149,25 @@ export default {
 .td-user-setting {
   width: 100%;
   height: 100%;
-  align-items: flex-start;
-  padding: var(--padding);
+  justify-content: space-around;
   gap: var(--padding);
+  .divide {
+    width: var(--padding);
+    height: 100%;
+    background-color: var(--bg-layer-color);
+    border-radius: var(--border-radius);
+  }
   .setting-group {
     width: 100%;
+    height: 100%;
     gap: var(--padding);
+    justify-content: flex-start;
     .user-setting-item {
       width: 100%;
       justify-content: space-between;
+      .input-setting {
+        width: 200px;
+      }
     }
   }
 }
