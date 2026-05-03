bdd75dae7672cefae89f868a4629ce27b2fb0b50
tonguyenducmanh <ducmanh14032000@gmail.com>
2026-05-03 20:44:48 +0700

    xóa bước copy config, sau này file config sẽ được gen tự động bởi code

--- build_all.sh
+++ build_all.sh
@@ -6,10 +6,3 @@ chmod 777 ./scripts/build_api.sh
 # ./scripts/build_api.sh
 chmod 777 ./scripts/build_daemon.sh
 ./scripts/build_daemon.sh
-
-# Copy toàn bộ nội dung config vào output
-ROOT_DIR=$(pwd)
-OUTPUT_DIR="$ROOT_DIR/out"
-CONIG_DIR="$ROOT_DIR/config"
-
-cp -r "$CONIG_DIR"/. "$OUTPUT_DIR"/
