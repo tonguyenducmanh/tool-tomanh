4ba8ffacedcc81b5ca8b20b78e60683ddf3f16d9
tonguyenducmanh <ducmanh14032000@gmail.com>
2026-06-12 23:05:26 +0700

    update build config

--- scripts/build_daemon.sh
+++ scripts/build_daemon.sh
@@ -4,15 +4,29 @@ set -e
 echo "--- Bắt đầu quy trình daemon ---"
 ROOT_DIR=$(pwd)
 
-# 1. Cấu hình đường dẫn
+# ĐỌC VERSION TỪ PACKAGE.JSON
+PACKAGE_JSON_PATH="$ROOT_DIR/package.json"
+
+if [ ! -f "$PACKAGE_JSON_PATH" ]; then
+    echo "Lỗi: Không tìm thấy file package.json tại $ROOT_DIR"
+    exit 1
+fi
+
+# đọc ra version hiện tại
+VERSION=$(node -e "echo(require('$PACKAGE_JSON_PATH').version);" 2>/dev/null || node -e "console.log(require('$PACKAGE_JSON_PATH').version);")
+
+echo "Phiên bản hiện tại: $VERSION"
+
+# Cấu hình đường dẫn
 DAEMON_DIR="$ROOT_DIR/src_backend/td_app/cmd/daemon_app"
 WEB_APP_DIR="$ROOT_DIR/src_backend/td_core_service/internal/web/dist/"
 FRONTEND_DIST="$ROOT_DIR/dist"
 OUTPUT_DIR="$ROOT_DIR/out"
-OUTPUT_NAME="dev-tool"
+
+OUTPUT_NAME="dev-tool-$VERSION"
 
 
-# 4. Build Backend (Go daemon)
+# Build Backend (Go daemon)
 echo "Đang build Go daemon..."
 cd "$DAEMON_DIR"
 
@@ -28,7 +42,8 @@ echo "Building for Windows..."
 GOOS=windows GOARCH=amd64  \
 go build -o "$OUTPUT_DIR/$OUTPUT_NAME-window.exe" .
 
+# Trở về thư mục gốc để xóa dist an toàn
+cd "$ROOT_DIR"
 rm -rf "$FRONTEND_DIST"
 
-
-echo "Build thành công!"
+echo "Build thành công phiên bản $VERSION!"
