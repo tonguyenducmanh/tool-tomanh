112a8ee3b36fe7486d33af4c438a7ab070eb3310
tonguyenducmanh <ducmanh14032000@gmail.com>
2026-06-23 19:27:02 +0700

    bổ sung thêm effect

--- src/components/TDDynamicBackgroundEffect.vue
+++ src/components/TDDynamicBackgroundEffect.vue
@@ -1,31 +1,31 @@
-<!-- component wrap việc hiển thị mỗi effect 1 ngày -->
 <template>
   <component :is="currentComponent" />
 </template>
 
-<script>
+<script setup>
+import { computed } from "vue";
 import TDCodeMoveEffect from "./backgroundEffect/TDCodeMoveEffect.vue";
-import TDParticleShapeEffect from "./backgroundEffect/TDParticleShapeEffect.vue";
 import TDGridLinesEffect from "./backgroundEffect/TDGridLinesEffect.vue";
 import TDPixelTunnelEffect from "./backgroundEffect/TDPixelTunnelEffect.vue";
+import TDParticleShapeEffect from "./backgroundEffect/TDParticleShapeEffect.vue";
+import TDRetro3DGridEffect from "./backgroundEffect/TDRetro3DGridEffect.vue";
+
+defineOptions({
+  name: "TDDynamicBackgroundEffect",
+});
 
 const effects = [
   TDCodeMoveEffect,
   TDGridLinesEffect,
   TDPixelTunnelEffect,
   TDParticleShapeEffect,
+  TDRetro3DGridEffect,
 ];
 
-export default {
-  name: "TDDynamicBackgroundEffect",
-  computed: {
-    currentComponent() {
-      const now = new Date();
-      const day = Math.floor(
-        (now - new Date(now.getFullYear(), 0, 0)) / 86400000,
-      );
-      return effects[day % effects.length];
-    },
-  },
-};
+const currentComponent = computed(() => {
+  const now = new Date();
+  const hours = now.getHours(); // Lấy số giờ hiện tại từ 0 - 23
+
+  return effects[hours % effects.length];
+});
 </script>
