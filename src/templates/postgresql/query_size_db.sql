8e00860fdf8cecb46d70692976155db06a7e1d8d
to nguyen duc manh <ducmanh14032000@gmail.com>
2026-06-30 17:28:11 +0700

    sửa lại 1 số script chưa tối ưu

--- src/templates/postgresql/query_size_db.sql
+++ src/templates/postgresql/query_size_db.sql
@@ -1,10 +1,5 @@
-WITH input AS (
-  SELECT current_database()::text AS db_name
-)
-SELECT
-  i.db_name AS database_name,
-  pg_database_size(i.db_name) AS size_in_bytes,
-  pg_database_size(i.db_name) / 1024 / 1024 AS size_in_mb,
-  pg_size_pretty(pg_database_size(i.db_name)) AS size_pretty
-FROM input i
-LIMIT 1;
+SELECT 
+    pg_database_size(pd.datname)/1024/1024 AS size_in_mb
+FROM 
+    pg_database pd where pd.datname = current_database() 
+limit 1;
--- src/templates/postgresql/query_size_table.sql
+++ src/templates/postgresql/query_size_table.sql
@@ -1,11 +1,7 @@
-WITH input AS (
-  SELECT
-    'public'::text AS schema_name,
-    'your_table_name'::text AS table_name
-)
-SELECT
-  i.schema_name || '.' || i.table_name AS table_full_name,
-  pg_total_relation_size(quote_ident(i.schema_name) || '.' || quote_ident(i.table_name)) AS size_in_bytes,
-  pg_size_pretty(pg_total_relation_size(quote_ident(i.schema_name) || '.' || quote_ident(i.table_name))) AS size_pretty
-FROM input i
-LIMIT 1;
+SELECT 
+    pg_size_pretty(
+        pg_total_relation_size(
+            'sme.account_suggestion_voucher_ba_ca'
+        )
+    ) 
+limit 1;
