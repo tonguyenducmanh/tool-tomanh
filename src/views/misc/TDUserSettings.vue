69f4793873b31dbda19ec5a8ff0f7afef72213bd
tonguyenducmanh <ducmanh14032000@gmail.com>
2026-08-01 07:00:15 +0700

    sửa lại design của màn hình thiết lập

--- src/views/misc/TDUserSettings.vue
+++ src/views/misc/TDUserSettings.vue
@@ -1,105 +1,87 @@
 <template>
-  <div class="flex td-user-setting">
-    <div class="flex flex-col setting-group">
-      <div class="flex user-setting-item">
-        <div>{{ $t("i18nCommon.apiTesting.tooltipUrlAgent") }}</div>
-        <div class="input-setting">
-          <TDInput
-            v-model="currentUserSetting.agentURL"
-            v-tooltip="$t('i18nCommon.apiTesting.tooltipUrlAgent')"
+  <div class="flex flex-col td-user-setting">
+    <div class="flex setting-content">
+      <div class="flex flex-col setting-group">
+        <div class="flex user-setting-item">
+          <div>{{ $t("i18nCommon.apiTesting.tooltipUrlAgent") }}</div>
+          <div class="input-setting">
+            <TDInput
+              v-model="currentUserSetting.agentURL"
+              v-tooltip="$t('i18nCommon.apiTesting.tooltipUrlAgent')"
+              :noMargin="true"
+              :placeHolder="$t('i18nCommon.apiTesting.agentUrl')"
+            />
+          </div>
+        </div>
+        <div class="flex user-setting-item">
+          <div>{{ $t("i18nUserSettings.settings.theme") }}</div>
+          <TDComboBox
+            :width="200"
+            v-model="currentUserSetting.theme"
+            :options="themeOption"
             :noMargin="true"
-            :placeHolder="$t('i18nCommon.apiTesting.agentUrl')"
+          />
+        </div>
+        <div class="flex user-setting-item">
+          <div>{{ $t("i18nUserSettings.settings.loadingType") }}</div>
+          <TDComboBox
+            :width="200"
+            v-model="currentUserSetting.currentLoadingType"
+            :options="loadingOption"
+            :noMargin="true"
+          />
+        </div>
+        <div class="flex user-setting-item">
+          <div>{{ $t("i18nUserSettings.settings.language") }}</div>
+          <TDComboBox
+            :width="200"
+            v-model="currentUserSetting.currentLanguage"
+            :options="languageOption"
+            :noMargin="true"
           />
         </div>
       </div>
-      <div class="flex user-setting-item">
-        <div>{{ $t("i18nUserSettings.settings.theme") }}</div>
-        <TDComboBox
-          :width="200"
-          v-model="currentUserSetting.theme"
-          :options="themeOption"
-          :noMargin="true"
-        />
-      </div>
-      <div class="flex user-setting-item">
-        <TDCheckbox
-          :variant="$tdEnum.checkboxType.switch"
-          v-model="currentUserSetting.wrapTab"
-          :label="$t('i18nUserSettings.settings.wrapTab')"
-          :noMargin="true"
-        ></TDCheckbox>
-      </div>
-      <div class="flex user-setting-item">
-        <TDCheckbox
-          :variant="$tdEnum.checkboxType.switch"
-          v-model="currentUserSetting.showTabNumber"
-          :label="$t('i18nCommon.tabManager.showTabNumber')"
-          :noMargin="true"
-        ></TDCheckbox>
-      </div>
-      <div class="flex user-setting-item">
-        <TDCheckbox
-          :variant="$tdEnum.checkboxType.switch"
-          v-model="currentUserSetting.toastInHeader"
-          :label="$t('i18nUserSettings.settings.toastInHeader')"
-          :noMargin="true"
-        ></TDCheckbox>
-      </div>
-      <div class="flex user-setting-item">
-        <TDCheckbox
-          :variant="$tdEnum.checkboxType.switch"
-          v-model="currentUserSetting.showCursorTrail"
-          :label="$t('i18nUserSettings.settings.cursorTrail')"
-          :noMargin="true"
-        ></TDCheckbox>
-      </div>
-      <div class="flex user-setting-item">
-        <div>{{ $t("i18nUserSettings.settings.loadingType") }}</div>
-        <TDComboBox
-          :width="200"
-          v-model="currentUserSetting.currentLoadingType"
-          :options="loadingOption"
-          :noMargin="true"
-        />
-      </div>
-      <div class="flex user-setting-item">
-        <div>{{ $t("i18nUserSettings.settings.language") }}</div>
-        <TDComboBox
-          :width="200"
-          v-model="currentUserSetting.currentLanguage"
-          :options="languageOption"
-          :noMargin="true"
-        />
-      </div>
-      <TDButton
-        :noMargin="true"
-        @click="saveSetting"
-        :label="$t('i18nUserSettings.saveSetting')"
-      />
-    </div>
-    <div class="divide"></div>
-
-    <div class="flex flex-col setting-group">
-      <div class="flex user-setting-item">
-        <div>{{ $t("i18nCommon.apiTesting.pingAgent") }}</div>
-        <TDButton
-          :noMargin="true"
-          @click="heathCheck"
-          :type="$tdEnum.buttonType.secondary"
-          :label="$t('i18nCommon.ping')"
-        ></TDButton>
-      </div>
-      <div class="flex user-setting-item">
-        <div>{{ $t("i18nCommon.apiTesting.downloadAgent") }}</div>
-        <TDButton
-          :noMargin="true"
-          :type="$tdEnum.buttonType.secondary"
-          @click="goToSource"
-          :label="$t('i18nCommon.tdheader.goToSource')"
-          v-tooltip="$t('i18nCommon.apiTesting.toolTipDownloadAgent')"
-        />
+      <div class="divide"></div>
+      <div class="flex flex-col setting-group">
+        <div class="flex user-setting-item">
+          <TDCheckbox
+            :variant="$tdEnum.checkboxType.switch"
+            v-model="currentUserSetting.wrapTab"
+            :label="$t('i18nUserSettings.settings.wrapTab')"
+            :noMargin="true"
+          ></TDCheckbox>
+        </div>
+        <div class="flex user-setting-item">
+          <TDCheckbox
+            :variant="$tdEnum.checkboxType.switch"
+            v-model="currentUserSetting.showTabNumber"
+            :label="$t('i18nCommon.tabManager.showTabNumber')"
+            :noMargin="true"
+          ></TDCheckbox>
+        </div>
+        <div class="flex user-setting-item">
+          <TDCheckbox
+            :variant="$tdEnum.checkboxType.switch"
+            v-model="currentUserSetting.toastInHeader"
+            :label="$t('i18nUserSettings.settings.toastInHeader')"
+            :noMargin="true"
+          ></TDCheckbox>
+        </div>
+        <div class="flex user-setting-item">
+          <TDCheckbox
+            :variant="$tdEnum.checkboxType.switch"
+            v-model="currentUserSetting.showCursorTrail"
+            :label="$t('i18nUserSettings.settings.cursorTrail')"
+            :noMargin="true"
+          ></TDCheckbox>
+        </div>
       </div>
     </div>
+    <TDButton
+      :noMargin="true"
+      @click="saveSetting"
+      :label="$t('i18nUserSettings.saveSetting')"
+    />
   </div>
 </template>
 <script>
@@ -140,29 +122,12 @@ export default {
     me.processWhenCreated();
   },
   methods: {
-    async heathCheck() {
-      let me = this;
-      try {
-        let res = await new TDAgentAPI().heathCheck();
-        if (res && res.success && res.data) {
-          me.$tdToast.success(res.data);
-        } else {
-          me.$tdToast.success(res);
-        }
-      } catch (ex) {
-        me.$tdUtility.showErrorNotFoundAgentServer();
-      }
-    },
     async processWhenCreated() {
       let me = this;
       let cacheData = await me.$tdUtility.getUserSettings();
       if (cacheData) {
         me.currentUserSetting = Object.assign(me.currentUserSetting, cacheData);
       }
-    },
-    goToSource() {
-      let me = this;
-      me.$tdUtility.goToSource();
     },
     async saveSetting() {
       let me = this;
@@ -194,24 +159,30 @@ export default {
 .td-user-setting {
   width: 100%;
   height: 100%;
-  justify-content: space-around;
+  justify-content: flex-start;
+  align-items: center;
   gap: var(--padding);
-  .divide {
-    width: var(--padding);
-    height: 100%;
-    background-color: var(--bg-layer-color);
-    border-radius: var(--border-radius);
-  }
-  .setting-group {
+  .setting-content {
     width: 100%;
-    height: 100%;
     gap: var(--padding);
-    justify-content: flex-start;
-    .user-setting-item {
+    .divide {
+      width: var(--padding);
+      height: 100%;
+      background-color: var(--bg-layer-color);
+      border-radius: var(--border-radius);
+    }
+    .setting-group {
       width: 100%;
-      justify-content: space-between;
-      .input-setting {
-        width: 200px;
+      height: 100%;
+      gap: var(--padding);
+      justify-content: flex-start;
+      .user-setting-item {
+        width: 100%;
+        justify-content: space-between;
+        .input-setting {
+          width: 200px;
+        }
+        height: 100%;
       }
     }
   }
