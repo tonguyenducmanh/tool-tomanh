906469a3144fd2696921308630bb887b4ec04fb8
tonguyenducmanh <ducmanh14032000@gmail.com>
2026-09-05 11:26:39 +0700

    bổ sung thêm 1 số tip mới

--- src/i18n/en/i18nTip.js
+++ src/i18n/en/i18nTip.js
@@ -1,10 +1,12 @@
 export default {
   i18nTip: {
     list: [
-      "When typing code, right-click to use the features for each language",
+      "When using the text editor, right-click to use the features for each language",
       "You can duplicate the tool you are using by right-clicking the tool name in the list of tools in use",
       "When using the automation tool, click the view-documentation button and copy the documentation to prompt AI to write the automation script",
       "Some tools require a backend agent to run; click Help on the app header and select Download Agent",
+      "You can quickly sort the keys of a JSON object in the text editor by right-clicking and selecting JSON sort by key",
+      "While typing an SQL query, you can place the cursor on a table, view, or function name and press F12 to quickly open that object's DLL",
     ],
     nextTip: "Next tip",
   },
--- src/i18n/vi/i18nTip.js
+++ src/i18n/vi/i18nTip.js
@@ -1,10 +1,12 @@
 export default {
   i18nTip: {
     list: [
-      "Khi nhập liệu code, chuột phải để dùng các tính năng cho từng ngôn ngữ",
+      "Khi sử dụng text editor, chuột phải để dùng các tính năng cho từng ngôn ngữ",
       "Bạn có thể nhân bản tool đang sử dụng bằng cách chuột phải vào tên tool trên danh sách tool đang dùng",
       "Khi sử dụng tool tự động hóa, ấn vào nút xem tài liệu rồi copy tài liệu để prompt cho AI code hộ script tự động hóa",
       "Một số tool cần sử dụng agent backend để có thể chạy được, ấn vào trợ giúp trên header ứng dụng và chọn tải agent",
+      "Bạn có thể sắp xếp nhanh key của object JSON trong text editor bằng cách chuột phải chọn JSON sắp xếp theo key",
+      "Khi đang gõ câu query SQL, bạn có thể đặt con trỏ chuột vào tên bảng, tên view, tên function và ấn F12 để mở nhanh DLL của object đó",
     ],
     nextTip: "Tip tiếp theo",
   },
